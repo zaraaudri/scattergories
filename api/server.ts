2bd0e6f39556a413235ce058051e4fe694a74d51
gazimonir84 <gazimonir84@gmail.com>
2026-06-29 13:01:46 -0400

    update prisma setup

---
+++ api/server.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import { prisma } from "../lib/prisma";
+
+const app = express();
+app.use(express.json());
+
+
+// const topics = ["Animals", "Foods", "Countries", "Movies", "Sports"];
+// const letters = "ABCDEFGHIJKLMNOPRSTW".split("");
+
+// const letter = letters[Math.floor(Math.random() * letters.length)];
+// const topic = topics[Math.floor(Math.random() * topics.length)];
+// function randomItem(list) {
+//   return list[Math.floor(Math.random() * list.length)];
+// }
+
+// function randomRoomCode() {
+//     return Math.floor(Math.random() * 10000);
+// }
+// app.get("/", (req, res) => {
+//   res.send("Hello, game!");
+// }); 
+
+// app.post("/games", async (req, res) => {
+//   const roomCode = randomRoomCode();
+//   const letter = randomItem(letters);
+//   const topic = randomItem(topics);
+
+//   const game = await prisma.games.create({
+//     data: {
+//       roomCode: roomCode,
+//       letter: letter,
+//       topic: topic,
+//     },
+//   });
+
+//   res.json({
+//     roomCode: game.roomCode,
+//     letter: game.letter,
+//     topic: game.topic,
+//   });
+// });
+
+//testing
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
+
+
