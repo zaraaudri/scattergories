f826752480cbc53d39997ef04d19032a88c6b595
Nicholas Carter <nicholas1304@icloud.com>
2026-06-29 09:32:04 -0400

    random

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
- import express from "express";
+import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000"); 
-});
-//testing 
+app.listen(3000);
+console.log("Server running on http://localhost:3000");
+
+const topics = ["Animals", "Foods", "Countries", "Movies", "Sports"];
+const letters = "ABCDEFGHIJKLMNOPRSTW".split("");
+
+const letter = letters[Math.floor(Math.random() * letters.length)];
+const topic = topics[Math.floor(Math.random() * topics.length)];
